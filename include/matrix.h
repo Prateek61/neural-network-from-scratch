5e48f5d1c7f83a054f9b5bb3150c93ac216d0d59
Prateek61 <prateekpoudel61@gmail.com>
2023-12-20 17:37:22 +0545

    initial project setup

---
+++ include/matrix.h
@@ -0,0 +1,15 @@
+// File: include/matrix.h
+// Purpose: Header file for matrix.cpp.
+
+#pragma once
+
+#include <iostream>
+
+namespace nn
+{
+    class matrix
+    {
+    public:
+        void test();
+    };
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "matrix.h"
+
+int main()
+{
+    nn::matrix m;
+    m.test();
+    return 0;
+}
